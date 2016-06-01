430aa98ffd71c263cb67668f96ff209f4c8b4719
bfennimore <bfennimore@gmail.com>
2016-05-31 20:37:26 -0400

    Update update2.php

--- update2.php
+++ update2.php
@@ -1,9 +1,9 @@
 <?php
-include 'configs/settings.php';
+include "configs/settings.php";
 if ($_POST["j_entry"]) {
-	echo "Got it!";
+        echo "Got it!";
 } else {
-	echo "I do not got it.";
+        echo "I do not got it.";
 }
 $logentry = $_POST["j_entry"];
 $date_entry = date("Y-m-d G:i:s");
@@ -15,7 +15,8 @@ if ($conn->connect_error) {
 }
 $sql = "INSERT INTO journal (date_added, entry) VALUES('$date_entry','$logentry')";
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+    echo "<html><body>New record created successfully<br><br><a href='upload.html'>Go make more</a><br>";
+    echo "<a href='home.php'>Go to homepage instead</a></body></html>";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
