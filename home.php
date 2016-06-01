1040cc6daf3eb1d4630338591b5c285e99592fc2
bfennimore <bfennimore@gmail.com>
2016-05-31 21:09:14 -0400

    Update home.php
    Fixed typo in table header

--- home.php
+++ home.php
@@ -12,7 +12,7 @@ if ($result->num_rows > 0) {
 echo "<html><body>";
 echo "<a href='upload.html'>Add stuff here</a><br>";
 echo '<table border=1 style="width:100%">';
-echo "<tr><th>Date Added</th><th>Enrty info</th></tr>";
+echo "<tr><th>Date Added</th><th>Entry info</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<tr><td>" . $row["date_added"]. "</td><td><pre>" . $row["entry"]. "</pre></td></tr>";
